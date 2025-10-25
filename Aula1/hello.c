bd0ca38aa7e3c0e8f650ffe99fc7204df8de19fd
Drew Harrison <antifaforliberty@gmail.com>
2025-10-25 10:42:02 -0300

    Atualizando pasta

---
+++ Aula1/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //Biblioteca padrao de entrada e saida
+
+int main(){ //Main Loop funcao principal
+
+    printf("Ola, Mundo!\n"); //Comando de saida
+
+    return 0; //Retorna um valor de saida inteiro
+}
