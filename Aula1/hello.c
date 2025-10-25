6c4336eadc1847a93de418d9b9c67bd0a73d6b31
Drew Harrison <antifaforliberty@gmail.com>
2025-10-25 10:53:21 -0300

    Adiconando aula 2

--- Aula1/hello.c
+++ Aula1/hello.c
@@ -4,5 +4,5 @@ int main(){ //Main Loop funcao principal
 
     printf("Ola, Mundo!\n"); //Comando de saida
 
-    return 0; //Retorna um valor de saida inteiro
+    return 0; //Retorna um valor de saida
 }
---
+++ Aula2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &a);
+
+    printf("Digite o valor de B: ");
+    scanf("%d", &b);
+
+    printf("O Valor de A eh %d e o Valor de B eh %d \n", a, b);
+
+    return 0;
+}
